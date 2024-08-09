b0469a09206c5482acb259e8d2e48af352553c27
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-08-09 14:04:47 -0600

    footer

--- movie-streaming-site/movie-streaming-site.py
+++ movie-streaming-site/movie-streaming-site.py
@@ -31,6 +31,7 @@ def index():
     return rx.box(
         rx.box(
             rx.image(src="https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/8torndado.jpg", width="100vw", height="108vh"),
+            
             rx.box(
                 width="100vw",
                 height="108vh",
@@ -102,6 +103,69 @@ def index():
                 movie_card("The Garfield Move", "2024", "101 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/2garlf.jpg"),
                 movie_card("Inside Out 2", "2024", "1 hr 31 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/1inside_out_2.jpg"),
                 movie_card("The Super Mario Bros Movie", "2023", "93 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/0maro.jpg"),
+                rx.hstack(
+                    rx.vstack(
+                        rx.text(
+                            "API attribution:",
+                            font_size= "4vh",
+                            font_weight="900",
+                        ),
+                        rx.link(
+                            rx.image(
+                            "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg",
+                            height="4vh",
+
+                            ),
+                            href="https://www.themoviedb.org",
+                            
+                        ),
+                        rx.link(
+                            rx.image(
+                                "https://moviesapi.club/assets/images/logo.png",
+                                height="6vh",
+
+                            ),   
+                            href="https://moviesapi.club",
+                            
+                        ),
+                    ),
+                    rx.center(
+                        rx.text(
+                            "Movie Streaming Site",
+                            font_size= "9vh",
+                            font_weight="900",
+                        ),
+                        height="100%",
+                        width="100%"
+                    ),
+                    
+                    rx.center(
+                        rx.vstack(
+                            rx.text(
+                                "Made with: ",
+                                font_size= "4vh",
+                                font_weight="900",
+                            ),
+                            rx.link(
+                                rx.image(
+                                    "https://reflex.dev/logos/dark/reflex.svg",
+                                    height="4vh",
+                                ),
+                                href="https://reflex.dev",
+                                
+                            ),
+                        ),
+                        pading_right="5vw",
+                        height="100%",
+                        width="15vw"
+                    ),
+                    
+                    background_color="rgba(255,255,255,0.02)",
+                    left="-1.5vw",
+                    position="relative",
+                    width="100vw",
+                    height="20vh"
+                ),
                 spacing="1.5vh",
                 columns="4",
                 position="absolute",
