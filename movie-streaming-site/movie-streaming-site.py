e9712a8210da6263d8d3e58a16e20680b5769da3
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-08-09 12:20:56 -0600

    thingababoms

--- movie-streaming-site/movie-streaming-site.py
+++ movie-streaming-site/movie-streaming-site.py
@@ -3,110 +3,125 @@ import reflex as rx
 class State(rx.State):
     pass
 
-def movie_card(title: str, year: str, duration: str):
+def movie_card(title: str, year: str, duration: str, img:str = "https://via.placeholder.com/316x421"):
     return rx.box(
-        rx.image(src="https://via.placeholder.com/316x421", width="316px", height="421px"),
+        rx.image(src=img, width="17vw", height="47vh"),
         rx.vstack(
             rx.hstack(
-                rx.text(year, color="#C0C0C0", font_size="20px", font_style="italic"),
-                rx.text(duration, color="#C0C0C0", font_size="20px", font_style="italic", text_align="right"),
+                rx.text(year, color="#C0C0C0", font_size="2vh", font_style="italic"),
+                rx.text(duration, color="#C0C0C0", font_size="2vh", font_style="italic", text_align="right", width="100%"),
                 width="100%"
             ),
-            rx.heading(title, color="#F2F2F2", font_size="36px", font_weight="800"),
-            spacing="4px",
+            rx.heading(title, color="#F2F2F2", font_size="4vh", font_weight="800"),
+            spacing="0.5vh",
             align_items="flex-start",
-            padding="16px",
+            padding="1.7vh",
         ),
         padding="1vw",
-        width="370px",
-        height="555px",
+        width="19vw",
+        #height="60.4vh",
         bg="#1A1A1A",
-        border_radius="8px",
-        border="1px solid #D9D9D9",
+        border_radius="1vh",
+        #border="0.1vh solid #D9D9D9",
     )
+
+
 
 def index():
     return rx.box(
         rx.box(
-            rx.image(src="https://via.placeholder.com/1920x994", width="1920px", height="994px"),
+            rx.image(src="https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/8torndado.jpg", width="100vw", height="108vh"),
             rx.box(
-                width="1920px",
-                height="994px",
+                width="100vw",
+                height="108vh",
                 position="absolute",
                 left="0",
                 top="0",
                 bg="linear-gradient(180deg, rgba(18, 18, 18, 0) 0%, rgba(18, 18, 18, 0.25) 37%, rgba(18, 18, 18, 0.77) 67%, rgba(18, 18, 18, 0.90) 77%, #121212 93%)",
             ),
-            
+           
             rx.button(
                 "Watch Now",
-                rx.icon(tag="play", size=70),
+                rx.icon(tag="circle_play", size=70),
                 bg="#FFE066",
                 color="black",
-                font_size="48px",
+                font_size="5vh",
                 font_weight="800",
-                border_radius="1000px",
-                border="6px solid #2C2C2C",
-                padding="12px",
+                border_radius="9999px",
+                border="0.7vh solid #2C2C2C",
+                padding="1.5vh",
                 position="absolute",
-                left="130px",
-                top="650px",
-                width="462px",
-                height="149px"
+                left="7vw",
+                top="70vh",
+                width="24vw",
+                height="16vh"
             ),
             rx.heading(
                 "TORNADO MOVIE OR SOMETHING",
-                font_size="128px",
+                font_size="14vh",
                 font_weight="900",
                 color="white",
                 position="absolute",
-                left="130px",
-                top="197px",
-                width="830px",
-                height="265px",
-                line_height="128px"
+                left="7vw",
+                top="20vh",
+                width="43vw",
+                height="30vh",
+                line_height="14vh"
             ),
-            width="1920px",
-            height="994px",
+            width="100vw",
+            height="111vh",
             position="relative",
         ),
         rx.hstack(
-            rx.icon(tag="search", color="white"),
-            rx.input(placeholder="Search", color="white", font_size="32px", width="100%", type="ghost"),
+            rx.icon(tag="search", color="white", size=30),
+            rx.input(
+                placeholder="Search",
+                color="white",
+                font_size="3.5vh",
+                width="100%",
+                background_color="rgba(255, 255, 255, 0)",
+                border="none",
+                border_width="0px",
+                height="30"
+            ),
             bg="rgba(0, 0, 0, 0.29)",
-            border_radius="9999px",
-            padding="6px",
-            width="924px",
+            border_radius="1087vh",
+            padding="0.5vh",
+            width="48vw",
             position="absolute",
             left="28vw",
             top="2vh",
         ),
-        rx.grid(
-            movie_card("Borderlands", "2024", "1 hr 31 min"),
-            movie_card("Dealpool & Wolverine", "2024", "1 hr 21 min"),
-            movie_card("Despicable Me 4", "2024", "1 hr 35 min"),
-            movie_card("Joe Rogan", "2024", "1 hr 23 min"),
-            movie_card("Trap", "2024", "2 hr 41 min"),
-            movie_card("The Garfield Move", "2024", "101 min"),
-            movie_card("Inside Out 2", "2024", "1 hr 31 min"),
-            movie_card("The Mario Movie", "2023", "93 min"),
-            spacing="24px",
-            columns="4",
-            position="absolute",
-            left="24px",
-            top="967px",
-            width="100%"
+        rx.center(
+            rx.grid(
+                movie_card("Borderlands", "2024", "1 hr 31 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/7borders.jpg"),
+                movie_card("Dealpool & Wolverine", "2024", "1 hr 21 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/3dead_and_wolv.jpg"),
+                movie_card("Despicable Me 4", "2024", "1 hr 35 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/4minions_wer_despicible.jpg"),
+                movie_card("Joe Rogan", "2024", "1 hr 23 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/6joe.jpg"),
+                movie_card("Trap", "2024", "2 hr 41 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/5trap.jpg"),
+                movie_card("The Garfield Move", "2024", "101 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/2garlf.jpg"),
+                movie_card("Inside Out 2", "2024", "1 hr 31 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/1inside_out_2.jpg"),
+                movie_card("The Super Mario Bros Movie", "2023", "93 min", "https://cloud-6t0bvxvfn-hack-club-bot.vercel.app/0maro.jpg"),
+                spacing="1.5vh",
+                columns="4",
+                position="absolute",
+                left="1.5vw",
+                top="105vh",
+                width="100%"
+            ),
+            width="100%",
         ),
+        
         bg="#121212",
         position="relative",
         overflow_x="hidden"
     )
-
 
 style = {
     "body":{
         "background-color":"#121212"
     }
 }
+
 app = rx.App(style = style)
 app.add_page(index)
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="movie",
+    app_name="movie-streaming-site",
 )
